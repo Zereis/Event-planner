5c04f59ed9b2c0744190727162715ebf91b78d82
David Hoang <62556785+Zereis@users.noreply.github.com>
2025-05-09 13:57:31 +0200

    first commit

--- src/pages/editEvent.jsx
+++ src/pages/editEvent.jsx
@@ -1,7 +1,7 @@
 // src/pages/editEvent.jsx
 
 import React, { useContext } from "react";
-import EditTask from "../Components/Edittask";
+import EditTask from "../Components/EditTask";
 import { TaskContext } from "../Components/TaskContext";
 
 const EditEvent = () => {
