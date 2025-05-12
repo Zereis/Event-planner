484c8a1f00516121a0fa941e2f6a5cd53d05b3eb
David Hoang <62556785+Zereis@users.noreply.github.com>
2025-05-12 10:19:59 +0200

    Revert "Develop david"

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-// filepath: c:\Users\David\Desktop\Event-planner\src\App.jsx
-import { Routes, Route } from 'react-router';
-import { TaskProvider } from './Components/TaskContext'; // Import TaskProvider
-import Home from './pages/Home';
-import './styles/app.css';
-import NavBar from './components/NavBar';
-import Spin from './components/Spin';
-import Calendar from './components/CalendarView';
-import Edit from './pages/Edit';
-import Add from './pages/Add';
-import Today from './pages/Today';
-import NoPages from './pages/NoPages';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <TaskProvider> {/* Wrap the entire app with TaskProvider */}
-      <div className="app-wrapper">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/today" element={<Today />} />
-          <Route path="/spin" element={<Spin />} />
-          <Route path="*" element={<NoPages />} />
-        </Routes>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </TaskProvider>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
